3930ed5d421a93cfc30243d423515369e1b40b0c
Sven Dockx <sven.dockx@gmail.com>
2022-05-23 09:42:28 +0200

    Ajoutez des fichiers projet.

---
+++ p4_server/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server
+{
+    class Program
+    {
+        private static Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static List<Socket> _clientSockets = new List<Socket>();
+        private static byte[] _buffer = new byte[1024];
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            SetupServer();
+            Console.ReadLine();
+        }
+
+        private static void SetupServer()
+        {
+            Console.WriteLine("Seting up server...");
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 10000));
+            _serverSocket.Listen(5);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void AcceptCallback(IAsyncResult AR) 
+        {
+            Socket socket = _serverSocket.EndAccept(AR);
+            _clientSockets.Add(socket);
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), null);
+        }
+
+        private static void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            int received = socket.EndReceive(AR);
+            byte[] dataBuf = new byte[received];
+            Array.Copy(_buffer, dataBuf, received);
+
+            string text = Encoding.ASCII.GetString(dataBuf);
+            Console.WriteLine("Text received: " + text);
+
+            string response = string.Empty;
+            response = DateTime.Now.ToLongTimeString();
+            byte[] data = Encoding.ASCII.GetBytes(response);
+
+            if (text.ToLower() == "get time")
+            {
+                socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+                socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            }
+            else if (text.ToLower() == "get time for all")
+            {
+                foreach (Socket sock in _clientSockets)
+                {
+                    sock.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), sock);
+                    sock.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), sock);
+                }
+            }
+            else
+            {
+                response = "Invalid Request";
+                data = Encoding.ASCII.GetBytes(response);
+                socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+                socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            }
+        }
+
+
+        private static void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+    }
+}
